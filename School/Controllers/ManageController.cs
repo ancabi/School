9c311a33fea5f7501db40761ffebd54b67dd587e
ancabi <ancabi@DESKTOP-8JK4F06>
2017-08-28 22:07:29 +0200

    Vista de usuarios, hijos OK Edit usuario funciona a falta de editar el tipo de usuario

--- School/Controllers/ManageController.cs
+++ School/Controllers/ManageController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages;
 using MySql.Data.MySqlClient;
 using school.Controllers;
 using school.Helpers;
@@ -20,6 +21,169 @@ namespace School.Controllers
 		{
 			return View();
 		}
+
+
+        [HttpPost]
+        public JsonResult LoadUsuarios()
+        {
+            RespGeneric resp = new RespGeneric("KO");
+            DataTable dt = new DataTable();
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(BD.CadConMySQL(BD.Server.BDLOCAL, BD.schema)))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM usuarios", con))
+                    {
+
+                        using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
+                        {
+
+                            da.Fill(dt);
+                            if (dt.Rows.Count > 0)
+                            {
+                                resp.cod = "OK";
+                                resp.d.Add("usuarios", dt.ToList());
+                            }
+                            else
+                            {
+                                resp.cod = "KO";
+                                resp.msg = "Ningún usuario encontrado!";
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                resp.cod = "KO";
+                resp.msg = e.Message;
+            }
+            return Json(resp);
+        }
+
+        [HttpPost]
+        public JsonResult LoadHijos(int idPadre)
+        {
+            RespGeneric resp = new RespGeneric("KO");
+            DataTable dt = new DataTable();
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(BD.CadConMySQL(BD.Server.BDLOCAL, BD.schema)))
+                {
+                    using (
+                        MySqlCommand cmd = new MySqlCommand("SELECT * FROM usuarios_hijos h WHERE idpadre=?idPadre", con)
+                    )
+                    {
+                        cmd.Parameters.AddWithValue("?idPadre", idPadre);
+                        using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
+                        {
+
+                            da.Fill(dt);
+                        }
+                    }
+                    if (dt.Rows.Count > 0)
+                            {
+
+                                List<Dictionary<string, object>> hijos = dt.ToList();
+                                dt.Reset();
+
+                                foreach (Dictionary<string, object> hijo in hijos)
+                                {
+                                    using (MySqlCommand cmd =new MySqlCommand("SELECT * FROM usuarios_hijos_deportes hd INNER JOIN deportes d ON d.id=hd.iddeporte WHERE idhijo=?idHijo",
+                                                con))
+                                    {
+                                        cmd.Parameters.AddWithValue("?idHijo", hijo["id"]);
+                                        using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
+                                        {
+                                            da.Fill(dt);
+                                        }
+                                    }
+
+                                    hijo.Add("deportes",dt.ToList());
+                                }
+
+                                resp.cod = "OK";
+                                resp.d.Add("hijos", hijos);
+                            }
+                            else
+                            {
+                                resp.cod = "KO";
+                                resp.msg = "Ningún hijo encontrado!";
+                            }
+                        }
+                    
+                
+            }
+            catch (Exception e)
+            {
+                resp.cod = "KO";
+                resp.msg = e.Message;
+            }
+            return Json(resp);
+        }
+
+        [HttpPost]
+        public JsonResult saveUsuario(int id, string dni, string nombre, string apellidos, string telefono, string telefonoAlt,string email, string usuario, string password,
+            string autorizacion,int tipo)
+        {
+
+            RespGeneric resp = new RespGeneric("KO");
+
+            try
+            {
+                string query = "";
+
+                if (password.IsEmpty())
+                {
+                    query =
+                        "UPDATE usuarios SET dni = ?dni, nombre = ?nombre,apellidos = ?apellidos,telefono = ?telefono,email = ?email,usuario = ?usuario, " +
+                        "tipo = ?tipo, autorizacion=?autorizacion WHERE id = ?id;";
+                }
+                else
+                {
+                    query =
+                        "UPDATE usuarios SET dni = ?dni,nombre = ?nombre,apellidos = ?apellidos,telefono = ?telefono,telefonoalt = ?telefonoAlt,email = ?email,usuario = ?usuario,password = ?password," +
+                        "tipo = ?tipo, autorizacion=?autorizacion WHERE id = ?id;";
+                }
+
+                using (MySqlConnection con = new MySqlConnection(BD.CadConMySQL(BD.Server.BDLOCAL, BD.schema)))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand(query, con))
+                    {
+
+                        cmd.Parameters.AddWithValue("?id", id);
+                        cmd.Parameters.AddWithValue("?dni", dni);
+                        cmd.Parameters.AddWithValue("?nombre", nombre);
+                        cmd.Parameters.AddWithValue("?apellidos", apellidos);
+                        cmd.Parameters.AddWithValue("?telefono", telefono);
+                        cmd.Parameters.AddWithValue("?email", email);
+                        cmd.Parameters.AddWithValue("?usuario", usuario);
+                        if (!password.IsEmpty())
+                        {
+                            String salt = BD.CreateSalt(8);
+                            String hashPass = BD.HashPassword(password, salt);
+                            cmd.Parameters.AddWithValue("?password", hashPass);
+                        }
+                        cmd.Parameters.AddWithValue("?tipo", tipo);
+                        cmd.Parameters.AddWithValue("?autorizacion", autorizacion);
+                        
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+
+                resp.cod = "OK";
+
+                return Json(resp);
+            }
+            catch (Exception e)
+            {
+                resp.msg = e.Message;
+
+                return Json(resp);
+            }
+        }
 
     }
 }
--- School/app/controllers/usuariosCtrl.js
+++ School/app/controllers/usuariosCtrl.js
@@ -1,40 +1,217 @@
-﻿angular.module('school').controller('usuariosCtrl', ['$scope', '$http', '$window', usuariosCtrl]);
+﻿angular.module('school').controller('usuariosCtrl', ['$scope', '$http', '$window', '$modal', usuariosCtrl]);
 
-function usuariosCtrl($scope, $http, $window) {
+function usuariosCtrl($scope, $http, $window, $modal) {
     var vm = this;
 
     //Variables
-    vm.usuario = "";
-    vm.pass = "";
     vm.loading = false;
     //////////////////////////////
 
     //Funciones
-    vm.login = login;
+    vm.loadHijos = loadHijos;
+    vm.editUsuario = editUsuario;
     //////////////////////////////
 
     //INIT
-    $window.sessionStorage.clear();
+    loadUsuarios();
     //////////////////////////////
 
-    function login() {
-        if (vm.usuario && vm.pass) {
-            vm.loading = true;
-            $http.post(webroot + "Account/LoginUser", { usuario: vm.usuario, pass: vm.pass })
-                .then(function (response) {
-                    if (response.data.cod === "OK") {
-                        $window.sessionStorage.webroot = webroot;
-                        $window.sessionStorage.usuario = response.data.d.usuario;
-                        $window.sessionStorage.tipo = response.data.d.tipo;
-                        $window.sessionStorage.cif = response.data.d.cif;
-                        $window.sessionStorage.ip_cliente = response.data.d.ip_cliente;
-                        $window.sessionStorage.nombre = response.data.d.nombre;
-                        $window.location.href = response.data.d.url;
-                    } else {
-                        vm.loading = false;
-                        swal({ title: 'Oops...', text: "Usuario o contraseña no validas", type: 'error' });
-                    }                    
-                });
-        }
+    function loadUsuarios() {
+        vm.loading = true;
+        $http.post(webroot + "Manage/LoadUsuarios", {})
+            .then(function (response) {
+                if (response.data.cod === "OK") {
+                    vm.usuarios = response.data.d.usuarios;
+                    vm.usuarios_disp = [].concat(response.data.d.usuarios);
+                    vm.loading = false;
+                } else {
+                    vm.loading = false;
+                    swal({ title: 'Oops...', text: response.data.msg, type: 'error' });
+                }
+            });
     }
+
+    function loadHijos() {
+        vm.loading = true;
+        $http.post(webroot + "Manage/LoadHijos", {idPadre:vm.usuarioSelected.id})
+            .then(function (response) {
+                if (response.data.cod === "OK") {
+                    vm.usuarioSelected.hijos = response.data.d.hijos;
+                    vm.loading = false;
+                } else {
+                    vm.loading = false;
+                    swal({ title: 'Oops...', text: response.data.msg, type: 'error' });
+                }
+            });
+    }
+
+    function editUsuario(item) {
+        $modal.open({
+            templateUrl: 'formUsuario.html',
+            controller: ModalInstanceCtrl,
+            controllerAs: 'vm',
+            windowClass: "usuario-modal",
+            resolve: {
+                item: function () { return item; }
+            }
+        }).result.then(function (result) {
+
+            loadUsuarios();
+
+        });
+    }
+
 }
+
+ModalInstanceCtrl.$inject = ['$scope', '$modalInstance', '$http', '$window', 'item'];
+function ModalInstanceCtrl($scope, $modalInstance, $http, $window,  item) {
+
+    //Variables
+    var vm = this;
+    vm.edit = false;
+    vm.session = $window.sessionStorage;
+    
+
+    if (item == null) {
+        vm.dni = "";
+        vm.nombre = "";
+        vm.apellidos = "";
+        vm.email = "";
+        vm.telefono = "";
+        vm.telefonoAlt = "";
+        vm.usuario = "";
+        vm.password = "";
+        vm.autorizacion = 0;
+
+        vm.edit = false;
+    } else {
+        vm.dni = item.dni;
+        vm.nombre = item.nombre;
+        vm.apellidos = item.apellidos;
+        vm.email = item.email;
+        vm.telefono = item.telefono;
+        vm.telefonoAlt = item.telefonoalt;
+        vm.usuario = item.usuario;
+        vm.password = "";
+        vm.autorizacion = item.autorizacion;
+
+        vm.edit = true;
+    }
+
+    //FUNCIONES
+    vm.aceptar = aceptar;
+    vm.cancelar = cancelar;
+    ///////////////////////////
+    //INIT
+    //loadPerfiles();
+
+    ////////////////////////////
+
+    
+
+    function loadPerfiles() {
+        $http.post(webroot + "Configuracion/loadPerfiles").then(function (response) {
+            vm.perfiles = response.data;
+
+            if (vm.edit) {
+                angular.forEach(vm.perfiles,
+                function (e) {
+                    if (e.id == item.idperfil) {
+                        vm.perfilSelected.push(e);
+                        e.ticked = true;
+                    }
+                });
+            }
+
+        });
+    }
+
+    function aceptar() {
+
+        var tipo = -1;
+        
+            //if (vm.perfilSelected.length != 0) {
+            //    tipo = vm.perfilSelected[0].id;
+            //}
+
+            if (!vm.edit) {
+                $http.post(webroot + "Configuracion/addUsuario",
+                {
+                    dni: vm.dni,
+                    nombre: vm.nombre,
+                    apellidos: vm.apellidos,
+                    telefono: vm.telefono,
+                    telefonoAlt: vm.telefonoAlt,
+                    email: vm.email,
+                    usuario: vm.usuario,
+                    password: vm.password,
+                    tipo: tipo,
+                    autorizacion:vm.autorizacion
+                }).then(function (response) {
+                    if (response.data.cod == "OK") {
+                        //toaster.pop({
+                        //    type: 'success',
+                        //    title: "Correcto",
+                        //    body: "Se ha añadido el usuario",
+                        //    showCloseButton: true
+                        //});
+                        $modalInstance.close();
+                    } else {
+                        //toaster.pop({
+                        //    type: 'error',
+                        //    title: "Error",
+                        //    body: response.data.msg,
+                        //    showCloseButton: true
+                        //});
+                    }
+                });
+            } else {
+                $http.post(webroot + "Manage/saveUsuario",
+                {
+                    id: item.id,
+                    dni: vm.dni,
+                    nombre: vm.nombre,
+                    apellidos: vm.apellidos,
+                    telefono: vm.telefono,
+                    telefonoAlt: vm.telefonoAlt,
+                    email: vm.email,
+                    usuario: vm.usuario,
+                    password: vm.password,
+                    tipo: tipo,
+                    autorizacion:vm.autorizacion
+                }).then(function (response) {
+                    if (response.data.cod == "OK") {
+                        //toaster.pop({
+                        //    type: 'success',
+                        //    title: "Correcto",
+                        //    body: "Se ha guardado el usuario",
+                        //    showCloseButton: true
+                        //});
+
+                        $modalInstance.close();
+                    } else {
+                        //toaster.pop({
+                        //    type: 'error',
+                        //    title: "Error",
+                        //    body: response.data.msg,
+                        //    showCloseButton: true
+                        //});
+                    }
+                });
+            }
+
+
+        
+
+
+
+    };
+
+    function cancelar() {
+
+        $modalInstance.dismiss('cancel');
+    };
+
+
+
+};
